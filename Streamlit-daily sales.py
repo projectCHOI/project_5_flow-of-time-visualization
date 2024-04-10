7385b99197ed32e470e5b97a92f5493c28df3fed
yoonsuk <yoonsuk.choi93@gmail.com>
2024-04-10 22:11:08 +0900

    TIME 형식 변환

--- Streamlit-daily sales.py
+++ Streamlit-daily sales.py
@@ -3,15 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 파일 불러오기
-file_path = 'C:/Users/HOME/Desktop/새싹_교육/GitHub_CHOI/project_5_flow-of-time-visualization/자료실/daily sales.csv'
+# 데이터 불러오기
+file_path = r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\project_5_flow-of-time-visualization\자료실\daily sales.csv'
 data = pd.read_csv(file_path)
 
 # Streamlit에서 파일 확인
 st.write(data)
 
-# 시간 데이터를 Datetime 형식으로 변환 (필요시)
-data['TIME'] = pd.to_datetime(data['TIME'])
+# 'TIME' 열의 데이터를 날짜/시간 형식으로 변환, 변환할 수 없는 값은 NaT으로 설정
+data['TIME'] = pd.to_datetime(data['TIME'], errors='coerce')
+
+# NaT 값이 포함된 행을 제거할 수 있음
+data = data.dropna(subset=['TIME'])
 
 # 그래프 그리기
 plt.figure(figsize=(10, 6))
