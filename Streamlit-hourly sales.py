88ee3e87c26932e8667f9d0215172bab2d5cae67
yoonsuk <yoonsuk.choi93@gmail.com>
2024-04-11 22:43:23 +0900

    hourly sales-원상복구

--- Streamlit-hourly sales.py
+++ Streamlit-hourly sales.py
@@ -3,38 +3,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# 데이터 불러오기
+# 파일 경로 설정
 file_path = r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\project_5_flow-of-time-visualization\자료실\hourly sales.csv'
-data = pd.read_csv(file_path)
 
-# 데이터 표시
-st.write("Daily Sales Data")
-st.dataframe(data)
+# Streamlit을 사용하여 데이터 불러오기
+@st.cache_data  # 데이터 캐싱을 위한 새로운 메소드 사용
+def load_data(path):
+    data = pd.read_csv(path)
+    return data
 
-# 일별 판매액 그래프 그리기
-st.write("Sales Graph")
-plt.figure(figsize=(10, 6))
-for column in ['SALES1', 'SALES2', 'SALES3', 'SALES4', 'SALES5', 'SALES6', 'SALES7']:
-    plt.plot(data['TIME'], data[column], label=column)
-plt.xlabel('Time')
-plt.ylabel('Sales')
-plt.legend()
-st.pyplot(plt)
+# 데이터 불러오기
+data = load_data(file_path)
 
-# 일별 판매액 히트맵
-st.write("Sales Heatmap")
-sales_data = data[['SALES1', 'SALES2', 'SALES3', 'SALES4', 'SALES5', 'SALES6', 'SALES7']]
-sns.heatmap(sales_data, cmap='Reds')
-st.pyplot(plt)
+# Streamlit에 데이터 표시
+st.write(data)
 
-# 'SUM' 열 계산 및 추가
-data['SUM'] = data[['SALES1', 'SALES2', 'SALES3', 'SALES4', 'SALES5', 'SALES6', 'SALES7']].sum(axis=1)
+# 그래프 그리기 준비
+sales_data = data[['TIME', 'SALES1', 'SALES2', 'SALES3', 'SALES4', 'SALES5', 'SALES6', 'SALES7']]
+sales_data = sales_data.set_index('TIME')
 
-# 시간별 판매 합계 꺽은선 그래프 시각화
-st.write("Sum of Sales Over Time")
-plt.figure(figsize=(10, 6))
-plt.plot(data['TIME'], data['SUM'], label='Sum of Sales')
-plt.xlabel('Time')
-plt.ylabel('Sum of Sales')
-plt.legend()
+# 히트맵 생성
+plt.figure(figsize=(10, 8))
+sns.heatmap(sales_data, cmap='Reds', linewidths=.5)
+plt.title('Hourly Sales Heatmap')
+plt.xlabel('Sales')
+plt.ylabel('Time')
+
+# Streamlit에 그래프 표시
 st.pyplot(plt)
