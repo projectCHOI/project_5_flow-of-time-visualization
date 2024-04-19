7207ca38a36803721735e6d2676ac051a0750da8
yoonsuk <yoonsuk.choi93@gmail.com>
2024-04-19 22:00:41 +0900

    pandas 다시

--- Streamlit-Total daily sales.py
+++ Streamlit-Total daily sales.py
@@ -1,18 +1,9 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+# 파일 경로 설정
+file_path = r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\project_5_flow-of-time-visualization\자료실\Master daily sales.csv'
 
-# 파일 읽기
-data = pd.read_csv(r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\project_5_flow-of-time-visualization\자료실\Master daily sales.csv')
+# CSV 파일 읽기
+data = pd.read_csv(file_path)
 
-# DataFrame 생성
-sales_df = pd.DataFrame(sales_data, index=[2016, 2017, 2018, 2019])
-
-# 히트맵 그리기
-plt.figure(figsize=(12, 8))
-sns.heatmap(sales_df, cmap='viridis', annot=True, fmt=".0f", linewidths=.5)
-plt.title('Monthly Sales Heatmap (2016-2019)')
-plt.xlabel('Month')
-plt.ylabel('Year')
-plt.show()
+# 데이터 확인
+print(data.head())
