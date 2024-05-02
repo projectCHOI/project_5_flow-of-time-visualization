fde26e923012f36ccd553233697fa3a0849fafce
yoonsuk <yoonsuk.choi93@gmail.com>
2024-05-02 15:14:58 +0900

    get_news_list : max_pages

--- project5_news-date.py
+++ project5_news-date.py
@@ -47,14 +47,18 @@ def get_news(url):
 def get_news_list(keyword, start_date, end_date):
     news = []
     dates = pd.date_range(start_date, end_date)
-
+# 페이지 수 제한
     for date in tqdm(dates, desc="Processing dates"):
         str_date = date.strftime("%Y.%m.%d")
-
         page = 1
+        max_pages = 1000  # 최대 페이지 제한
+
         while True:
             strdate = str_date.replace(".", "")
             start = (page - 1) * 10 + 1
+            if start > max_pages * 10:
+                break  # 1000페이지를 초과하면 다음 날짜로 넘어감
+# 페이지 수 제한
 
             headers = {"Cookie": "NNB=OURWEDJJKXKGI; nx_ssl=2; _naver_usersession_=+VdPHYAMq5W53YOSu5ttbQ==; page_uid=ieZzndprvmsssdTDFjRssssssah-332673"}
             URL = f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query={keyword}&sort=2&photo=0&field=0&pd=3&ds={str_date}&de={str_date}&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:r,p:from{strdate}to{strdate},a:all&start={start}"
