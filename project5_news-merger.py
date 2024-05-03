7bd5ad86c95e187ffbcc3b9bce350b7143e8e4ed
yoonsuk <yoonsuk.choi93@gmail.com>
2024-05-03 21:30:10 +0900

    news-merger

--- project5_news-merger.py
+++ project5_news-merger.py
@@ -1,1 +1,13 @@
 # news-merger
+import os
+import pandas as pd
+
+folder_path = '병합 대상 폴더'
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+
+data_frames = [pd.read_csv(os.path.join(folder_path, file)) for file in csv_files]
+merged_df = pd.concat(data_frames, ignore_index=True)
+
+merged_df.to_csv('저장 경로/파일이름.csv', index=False)
+
+print('CSV 병합 완료.')
